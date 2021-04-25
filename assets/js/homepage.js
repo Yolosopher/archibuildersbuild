9610c0c484ab0206b1629d80020bfd09869b4cb0
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2021-04-25 12:04:19 +0400

    homepagesec1 added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,20 +1,30 @@
-// const sections = document.querySelectorAll('.section')
+const returnYtImgUrl = (url) => {
+    let utubecode = url.split('/watch?v=')[1]
+    return `https://img.youtube.com/vi/${utubecode}/maxresdefault.jpg`
+}
+const returnYtEmbedUrl = (url) => {
+    let utubecode = url.split('/watch?v=')[1]
+    return `https://www.youtube.com/embed/${utubecode}`
+}
 
-// const callbackFunction = (entries) => {
-//     if (entries[0].isIntersecting) {
-//         entries[0].target.classList.add('observed')
-//     } else {
-//         entries[0].target.classList.remove('observed')
-//     }
-// }
 
-// const observer = new IntersectionObserver(callbackFunction, {
-//     threshold: 0.6
-// })
+const sections = document.querySelectorAll('.section')
 
-// sections.forEach(section => {
-//     observer.observe(section)
-// })
+const callbackFunction = (entries) => {
+    if (entries[0].isIntersecting) {
+        entries[0].target.classList.add('fadein')
+    } else {
+        entries[0].target.classList.remove('fadein')
+    }
+}
+
+const observer = new IntersectionObserver(callbackFunction, {
+    threshold: 0.3
+})
+
+sections.forEach(section => {
+    observer.observe(section)
+})
 let swp = new Swiper(".header__swiper", {
   slidesPerView: 1,
   speed: 1800,
@@ -40,35 +50,51 @@ let swp = new Swiper(".header__swiper", {
       setTimeout(() => {
           swp.el.classList.add('fadein')
       }, 1800);
-      //   swp.
-
-      // activeImages.classList.add('fadein')
-      // setTimeout(() => {
-      //     activeSlide.style.zIndex = ''
-      //     activeImages.classList.remove('fade-in')
-      // }, 1200);
-      // setTimeout(() => {
-      //     console.log('transitionEnd')
-      //     activeSlide.style.zIndex = ''
-      //     let newActiveSlide = document.querySelectorAll('.header__swiper__wrapper__slide')[swp.activeIndex]
-      //     newActiveSlide.style.zIndex = 10
-      //     newActiveSlide.querySelector('.header__swiper__wrapper__slide__images').classList.remove('fadein')
-      // }, 1200);
-      // activeSlideImages.classList.add('fadein')
-      // console.log('added')
-      // setTimeout(() => {
-      //   activeSlideImages.classList.remove('fadein')
-      //   console.log('removed')
-      // }, 600);
     },
   },
 });
 
 const paralaxbgs = document.getElementsByClassName("paralaxeff");
-
 new simpleParallax(paralaxbgs);
+
+
+const homevideo__video__img = document.querySelector('.homevideo__video__imgbox img')
+const homevideo__video__link = document.querySelector('.homevideo__video__link')
+
+
+const sec1videomodal = document.querySelector('.sec1videomodal')
+const modalvideocontainer = document.getElementById('modalvideocontainer')
+const modalClose = document.getElementById('modalClose')
+
+window.addEventListener('DOMContentLoaded', () => {
+    let link = homevideo__video__link.dataset.href
+    let imgLink = returnYtImgUrl(link)
+    homevideo__video__img.src = imgLink    
+
+    homevideo__video__link.addEventListener('click', () => {
+        const newIframe = document.createElement('iframe')
+        newIframe.title = 'YouTube video player'
+        newIframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
+        newIframe.setAttribute('allowfullscreen', true)
+        newIframe.setAttribute('frameborder', 0)
+        newIframe.src = returnYtEmbedUrl(link)
+        
+        modalvideocontainer.appendChild(newIframe)
+        
+        sec1videomodal.classList.add('opened')
+    })
+    modalClose.addEventListener('click', () => {
+        sec1videomodal.classList.remove('opened')
+        modalvideocontainer.innerHTML = ''
+    })
+})
 window.addEventListener('load', () => {
     [...document.getElementsByClassName('swiper-slide')].forEach(each => {
         each.classList.add('newtransdelay')
     })
 })
+
+// const sections = document.getElementsByClassName("section");
+// new simpleParallax(paralaxbgs);
+
+
